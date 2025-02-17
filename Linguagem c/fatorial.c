2ed019cb4c5dce2f518b7b396a7e2a80a835eb88
NayronCampos <Nayroncampossoares>
2025-02-17 15:11:37 -0300

    Atualização for e while

--- Linguagem c/fatorial.c
+++ Linguagem c/fatorial.c
@@ -5,11 +5,21 @@ int main (void){
 
     scanf("%d", &y);
 
+    for(int i=1;i<=y;i++){
+        x*=i;
+        
+    }
+    printf("%d\n", x);
+
+    x=1;
+
     while(y>0){
         
         x*=y;
         y--;
     }
-    printf("%d", x);
+    printf("%d\n", x);
+    
+
     return 0;
 }
