a58652da6656d73994ab1de5cad4e550fe549688
NayronCampos <Nayroncampossoares>
2025-02-18 11:52:11 -0300

    Interações em java

---
+++ AEDS II/lab01.java
@@ -0,0 +1,17 @@
+public class lab01 {
+
+    
+
+    public static void main(String[] args) {
+
+        var mensagem = "Fala polvo, a soma de 2 + 3 é:";
+        var total = 2+3;
+            imprimir(mensagem, total);
+    }
+     static void imprimir(String m, int t){
+
+        System.out.println(m);
+        System.out.println(t);
+        System.out.println("OOh bct\n");
+    }
+}
