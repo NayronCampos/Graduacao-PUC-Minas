fb3cb7f8a6b2dc801ca0386bd137d65301505280
NayronCampos <Nayroncampossoares>
2025-03-02 20:10:13 -0300

    Ciframento e alteração - q4

--- TPs/tp01_q03.java
+++ TPs/tp01_q03.java
@@ -1,5 +1,32 @@
-package TPs;
+
+import java.util.Scanner;
 
 public class tp01_q03 {
+    static String cifrar(String mensagem) {
+        String resultado = "";
+        int tamanho = mensagem.length();
 
+        for (int i = 0; i < tamanho; i++) {
+            char caractere = mensagem.charAt(i);
+            char cifrado = (char) (caractere + 3); // Desloca o caractere 3 posições para frente
+            resultado += cifrado; // Adiciona o caractere cifrado na nova string
+        }
+
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String mensagem = sc.nextLine().trim();
+
+            if (mensagem.equals("FIM"))
+                break; // Para o programa se a entrada for "FIM"
+
+            System.out.println(cifrar(mensagem));
+        }
+
+        sc.close();
+    }
 }
