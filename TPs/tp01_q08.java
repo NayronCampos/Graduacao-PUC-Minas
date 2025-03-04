1bf713f37e75a4ebd3a4deadc12e5fc1e99b931f
NayronCampos <Nayroncampossoares>
2025-03-04 10:20:42 -0300

    Q 07 e 08

---
+++ TPs/tp01_q08.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class tp01_q08 {
+
+    public static int somar(String numero) {
+        int i = 0;
+        int soma = 0;
+
+        // Usando o while para contar os caracteres da string
+        while (i < numero.length()) {
+            soma += numero.charAt(i) - '0'; // Converte o caractere para número e soma
+            i++;
+        }
+
+        return soma;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String valor = scanner.nextLine();
+            if (valor.isEmpty())
+                break;
+            System.out.println(somar(valor));
+        }
+    }
+}
