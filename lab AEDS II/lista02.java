5ec032f5e67c88e44b0e4e89c9c6294808adc806
NayronCampos <nayroncampossoares@gmail.com>
2025-03-11 13:44:07 -0300

    lista2

--- lab AEDS II/lista02.java
+++ lab AEDS II/lista02.java
@@ -1,12 +1,18 @@
-
+import java.util.Scanner;
 
 public class lista02 {
 
 
-
 public static void main (String [] args){
 
-    System.out.println("Hello World");
+    Scanner ler = new Scanner(System.in);
+ int max = ler.nextInt();
+
+  System.out.println(max);
+  
+ 
+
+   
 
 }
 
