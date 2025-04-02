77b259f23260465aa16438c0e320023ae7bd3da4
NayronCampos <nayroncampossoares@gmail.com>
2025-04-02 15:10:32 -0300

    Updates anexs

---
+++ javaScript/javascript.js
@@ -0,0 +1,10 @@
+   /*
+            Comandos de saída.
+            variável de entrada
+            template string
+        */
+
+            var nome = prompt("Send your name:");
+            document.write(`Hello, ${nome}`);
+            alert(`Hello, ${nome}`);
+            console.log(`Hello, ${nome}`);
