b08e51e829a9a1dc73b7a18ebf75a3707b34e6ae
NayronCampos <nayroncampossoares@gmail.com>
2025-04-09 15:12:01 -0300

    updates number 8

--- dia02/javaScript/javascript08.js
+++ dia02/javaScript/javascript08.js
@@ -7,5 +7,5 @@ var num2 = parseInt(document.getElementById("num2").value);
 var soma = num1 + num2;
 
 
-document.getElementById("resp").innerHTML += `${num1} + ${num2} = ${soma}`;
+document.getElementById("resp").innerHTML += `${num1} + ${num2} = ${soma}<br>`;
 }
