3f738fa2def6adb339f65626efb211c28fd7edaf
NayronCampos <nayroncampossoares@gmail.com>
2025-04-10 11:03:55 -0300

    Update to  close

--- TP2/tp02_q01.java
+++ TP2/tp02_q01.java
@@ -9,6 +9,10 @@ e cada uma cont´em um n´umero inteiro indicando o show id do Show a ser lido.
 A ´ultima linha da entrada cont´em a palavra FIM. A sa´ıda padr˜ao tamb´em cont´em v´arias
 linhas, uma para cada registro contido em uma linha da entrada padr˜ao, no seguinte formato: [=> id ## type ## title ## director ## [cast] ## country ## date added ##
 release year ## rating ## duration ## [listed in].*/
+import java.io.RandomAccessFile;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class tp02_q01{
@@ -17,10 +21,79 @@ public class Show{
     private:
     String SHOW_ID;
     String TYPE, TITLE, DIRECTOR, COUNTRY, RATING, DURATION;
-    Date DATE_ADDED;
-    int RELASE_YEAR
+    LocalDate DATE_ADDED;
+    int RELASE_YEAR;
     String[] CAST = new String[];
+    String[] LISTED_IN = new String[];
 
 }
+
+ public show() {
+        this.SHOW_ID = "";
+        this.TYPE = "";
+        this.TITLE = "";
+        this.DIRECTOR = "";
+        this.COUNTRY = "";
+        this.RATING = "";
+        this.DURATION = "";
+        this.DATE_ADDED = LocalDate.now();
+        this.RELASE_YEAR = 0;
+        this.CAST = new String[0];
+        this.LISTED_IN = new String[0];
+        
+    }
+
+    public show(String SHOW_ID, String TYPE, String TITLE, String DIRECTOR, String COUNTRY, String RATING, String DURATION, LocalDate DATE_ADDED, int RELASE_YEAR, String[] CAST, String[] LISTED_IN) {
+
+        this.SHOW_ID = SHOW_ID;
+        this.TYPE = TYPE;
+        this.TITLE = TITLE;
+        this.DIRECTOR = DIRECTOR;
+        this.COUNTRY = COUNTRY;
+        this.RATING = RATING;
+        this.DURATION = DURATION;
+        this.DATE_ADDED = DATE_ADDED;
+        this.RELASE_YEAR = RELASE_YEAR;
+        this.CAST = CAST;
+        this.LISTED_IN = LISTED_IN;
+
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String[] getAlternativeNames() { return alternativeNames; }
+    public String getHouse() { return house; }
+    public String getAncestry() { return ancestry; }
+    public String getSpecies() { return species; }
+    public String getPatronus() { return patronus; }
+    public Boolean getHogwartsStaff() { return hogwartsStaff; }
+    public Boolean getHogwartsStudent() { return hogwartsStudent; }
+    public String getActorName() { return actorName; }
+    public Boolean getAlive() { return alive; }
+    public LocalDate getDateOfBirth() { return dateOfBirth; }
+    public int getYearOfBirth() { return yearOfBirth; }
+    public String getEyeColour() { return eyeColour; }
+    public String getGender() { return gender; }
+    public String getHairColour() { return hairColour; }
+    public Boolean getWizard() { return wizard; }
+    
+    // Métodos set
+    public void setId(String id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setAlternativeNames(String[] alternativeNames) { this.alternativeNames = alternativeNames; }
+    public void setHouse(String house) { this.house = house; }
+    public void setAncestry(String ancestry) { this.ancestry = ancestry; }
+    public void setSpecies(String species) { this.species = species; }
+    public void setPatronus(String patronus) { this.patronus = patronus; }
+    public void setHogwartsStaff(Boolean hogwartsStaff) { this.hogwartsStaff = hogwartsStaff; }
+    public void setHogwartsStudent(Boolean hogwartsStudent) { this.hogwartsStudent = hogwartsStudent; }
+    public void setActorName(String actorName) { this.actorName = actorName; }
+    public void setAlive(Boolean alive) { this.alive = alive; }
+    public void setDateOfBirth(LocalDate dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+    public void setYearOfBirth(int yearOfBirth) { this.yearOfBirth = yearOfBirth; }
+    public void setEyeColour(String eyeColour) { this.eyeColour = eyeColour; }
+    public void setGender(String gender) { this.gender = gender; }
+    public void setHairColour(String hairColour) { this.hairColour = hairColour; }
+    public void setWizard(Boolean wizard) { this.wizard = wizard; }
 
 }
