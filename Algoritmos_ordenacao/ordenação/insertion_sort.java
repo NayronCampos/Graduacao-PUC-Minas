667074d7d30739f57131c79881d814fe5af2e542
NayronCampos <nayroncampossoares@gmail.com>
2025-04-16 18:28:37 -0300

    selection and isertion

---
+++ Algoritmos_ordenacao/ordenação/insertion_sort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class insertion_sort {
+    
+public static void ordenacao(int[] vetor, int x){
+
+for(int i=1;i<x;i++){
+    int tmp = vetor[i];
+    int j=i-1;
+    while((j>=0) && (vetor[j]>tmp)){
+        vetor[j+1] = vetor[j];
+        j--;
+    }
+    vetor[j+1] = tmp;
+}
+for(int i=0;i<x;i++){
+    System.out.println(vetor[i]); 
+}
+
+}
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] valores = new int[n];
+
+        for(int i=0;i<n;i++){
+            valores[i] = scanner.nextInt();
+        }
+
+        ordenacao(valores, n);
+    }
+}
