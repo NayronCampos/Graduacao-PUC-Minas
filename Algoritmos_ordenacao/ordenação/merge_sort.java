73f56d2a82f49db6d0cfe5f57c66adf1e7286278
NayronCampos <nayroncampossoares@gmail.com>
2025-04-17 11:34:43 -0300

    merge don't finish

--- Algoritmos_ordenacao/ordenação/merge_sort.java
+++ Algoritmos_ordenacao/ordenação/merge_sort.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class merge_sort {
+    
+    public static void ordenacao(int[] vetor, int x){
+
+        
+        for(int i=0;i<x;i++){
+            System.out.println(vetor[i]); 
+        }
+        
+        }    public static void main(String[] args){
+                Scanner scanner = new Scanner(System.in);
+        
+                int n = scanner.nextInt();
+                int[] valores = new int[n];
+        
+                for(int i=0;i<n;i++){
+                    valores[i] = scanner.nextInt();
+                }
+        
+                ordenacao(valores, n);
+            }
+
     
 }
