fc7b7bcda48def5f994447538e29b295f0c7db2b
NayronCampos <nayroncampossoares@gmail.com>
2025-05-11 21:51:25 -0300

    update

--- lab_ex/ex2.c
+++ lab_ex/ex2.c
@@ -1,18 +1,78 @@
-/*A entrada é composta por um número inteiro P, representando o ponto cardeal do avião que entrou no campo da pista (-4 <= P <= -1), onde (-4 representa o lado leste, -3 o lado norte, -2 lado sul e -1 lado oeste) . Em seguida é realizada a entrada dos respectivos aviões, compostos de um identificador começando com a letra “A” seguida de um número inteiro I (1 <= I <= 1000). A qualquer momento é permitido trocar o ponto cardeal, e inserir novas aeronaves, repetidamente até que o controlador finalize a sessão com o dígito 0.*/
-
 #include <stdio.h>
-int main (void){
-    int i=0;
-    char entrada[1000];
-    char leste[1000];
-    char norte[1000];
-    char sul[1000];
-    char oeste[1000];
-    
-    for(int i=0; entrada!= '0', i++){
-        scanf("%s", &entrada[i]);
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_AVIOES 10000
+#define TAM_ID        10
+
+int main() {
+
+    char oeste[MAX_AVIOES][TAM_ID];
+    char norte[MAX_AVIOES][TAM_ID];
+    char sul[MAX_AVIOES][TAM_ID];
+    char leste[MAX_AVIOES][TAM_ID];
+    int co = 0, cn = 0, cs = 0, cl = 0;
+
+    char token[TAM_ID];
+    int direcaoAtual = 0;
+
+    while (scanf("%s", token) == 1) {
+
+        if (strcmp(token, "0") == 0) {
+            break;
+        }
+        // Se começa com ‘-’, é um ponto cardeal
+        if (token[0] == '-') {
+            direcaoAtual = atoi(token);
+            continue;
+        }
+
+
+        if (direcaoAtual == -1 && co < MAX_AVIOES) {
+            strcpy(oeste[co++], token);
+        }
+        else if (direcaoAtual == -3 && cn < MAX_AVIOES) {
+            strcpy(norte[cn++], token);
+        }
+        else if (direcaoAtual == -2 && cs < MAX_AVIOES) {
+            strcpy(sul[cs++], token);
+        }
+        else if (direcaoAtual == -4 && cl < MAX_AVIOES) {
+            strcpy(leste[cl++], token);
+        }
 
     }
-    
-    
+
+
+
+    int maxLinhas = co;
+    if (cn > maxLinhas) maxLinhas = cn;
+    if (cs > maxLinhas) maxLinhas = cs;
+    if (cl > maxLinhas) maxLinhas = cl;
+
+    int primeiro = 1;
+    for (int i = 0; i < maxLinhas; i++) {
+        if (i < co) {
+            if (!primeiro) printf(" ");
+            printf("%s", oeste[i]);
+            primeiro = 0;
+        }
+        if (i < cn) {
+            if (!primeiro) printf(" ");
+            printf("%s", norte[i]);
+            primeiro = 0;
+        }
+        if (i < cs) {
+            if (!primeiro) printf(" ");
+            printf("%s", sul[i]);
+            primeiro = 0;
+        }
+        if (i < cl) {
+            if (!primeiro) printf(" ");
+            printf("%s", leste[i]);
+            primeiro = 0;
+        }
+    }
+    printf("\n");
+    return 0;
 }
