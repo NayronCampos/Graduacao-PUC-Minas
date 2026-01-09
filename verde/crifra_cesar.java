6323b15e842d70c0b4cd1734716cb397c3a3ee8d
NayronCampos <nayroncampossoares@gmail.com>
2026-01-08 21:35:27 -0300

    finish cirfra

--- verde/crifra_cesar.java
+++ verde/crifra_cesar.java
@@ -3,16 +3,37 @@ import java.util.Scanner;
 
 public class crifra_cesar {
 
+    static String cesar(String pala, int tam) throws Exception{
+         if(pala.length()<3){
+            throw new Exception("Palavra não pode ser menor que 3");
+        }
+        
+        if(tam<0){
+            return "";
+        }
 
-    public static void main(String args[]){
+        char letracifrada = (char) (pala.charAt(tam) + 3);
+
+        return cesar(pala, tam - 1) + letracifrada;
+    }
+
+    public static void main(String args[]) throws Exception {
         Scanner scan = new Scanner(System.in);
-            String palavra;
 
-            palavra = scan.nextLine();
+
+            String palavra, newPala;
             
+            palavra = scan.nextLine();
+
+            int tam = palavra.length();
+
+            newPala = cesar(palavra, tam-1);
+
+            System.out.println(newPala);
 
 
 
+        scan.close();
     }
     
 }
